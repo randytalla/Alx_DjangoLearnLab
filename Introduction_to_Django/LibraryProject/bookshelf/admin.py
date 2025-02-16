c5ba1cf9da254502d06885792086542acfa35550
randytalla <randytala2@gmail.com>
2025-02-16 11:16:47 -0800

    did the last task

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns to display
+    list_filter = ('publication_year',)  # Sidebar filter by publication year
+    search_fields = ('title', 'author')  # Enables search by title & author
+
+admin.site.register(Book, BookAdmin)
--- Introduction_to_Django/LibraryProject/bookshelf/create.md
+++ Introduction_to_Django/LibraryProject/bookshelf/create.md
@@ -1,8 +1,4 @@
 # Create a book
 Book.objects.create(title=“1984”, author= “George Orwell”, publication_year=1949)
 
-# Get all books
-all_books = Book.objects.all()
-print(all_books)  
 
-# Output: <QuerySet [<Book: The Alchemist>]>
