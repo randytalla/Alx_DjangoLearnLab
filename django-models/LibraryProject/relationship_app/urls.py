2b86cfd9c5d9a1f2d22695f962a369664804e730
randytalla <randytala2@gmail.com>
2025-02-21 23:08:01 -0800

    added the url configuration for the register, logout and login view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import list_books  # ✅ Import the function-based view
 from .views import LibraryDetailView  
+from .views import user_login, user_logout, register
 
 urlpatterns = [
     path('books/', list_books, name='list-books'),# ✅ Add route for the books list
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path("register/", register, name="register"),  # User Registration
+    path("login/", user_login, name="login"),  # User Login
+    path("logout/", user_logout, name="logout"),  # User Logout
 ]
