bc1cdcda2f2ca0d8864a017f965ce2595c45b611
randytalla <randytala2@gmail.com>
2025-02-21 20:11:39 -0800

    corrected my import file for class base view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book 
