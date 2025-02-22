307ba21e1f58b507e1e08264ac05662c65ea4bf5
randytalla <randytala2@gmail.com>
2025-02-21 23:02:14 -0800

    created the logic, logout and register view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,11 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Book 
 from .models import Library
+
 
 
 # Create your views here.
@@ -14,3 +17,32 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# 📌 User Registration View (Sign Up)
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")  # Redirect to login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+# 📌 User Login View
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("book-list")  # Redirect to book list after login
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")  # Redirect to login page after logout
+
