bf72879f7950017ca9e336ad9ba9c56080588d7d
randytalla <randytala2@gmail.com>
2025-02-21 19:35:32 -0800

    created both the class and function view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Library
+from django.views.generic import DetailView
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    book_info = "\n".join([f"{book.title} by {book.author.name}" for book in books])  # Format as text
+    return HttpResponse(book_info)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
