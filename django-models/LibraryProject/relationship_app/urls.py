930424734c1c78cbf939098067f3783ca42e9afb
randytalla <randytala2@gmail.com>
2025-02-21 23:54:44 -0800

    correct the view.register file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,3 +1,4 @@
+from django import views
 from django.urls import path
 from .views import list_books  # ✅ Import the function-based view
 from .views import LibraryDetailView  
@@ -8,7 +9,7 @@ from django.contrib.auth.views import LoginView, LogoutView, RegisterView
 urlpatterns = [
     path('books/', list_books, name='list-books'),# ✅ Add route for the books list
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path('register/', RegisterView.as_view(), name='register'),  # User Registration
-    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),  # Built-in login
-    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),  # Built-in logout
+    path("register/", views.register, name="register"),  # User Registration
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),  # User Login
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),  # User Logout  # Built-in logout
 ]
