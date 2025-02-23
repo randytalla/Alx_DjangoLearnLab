fdbbbe7c0ca5c852e00543d62cc37bc946d4f3b0
randytalla <randytala2@gmail.com>
2025-02-23 10:32:09 -0800

    did some modifications

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -24,31 +24,29 @@ from .models import Library
 
 # Role Check Functions
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    if user.is_authenticated:
+        try:
+            return user.userprofile.role == 'Admin'
+        except ObjectDoesNotExist:
+            return False
+    return False
 
 def is_librarian(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    if user.is_authenticated:
+        try:
+            return user.userprofile.role == 'Librarian'
+        except ObjectDoesNotExist:
+            return False
+    return False
 
 def is_member(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    if user.is_authenticated:
+        try:
+            return user.userprofile.role == 'Member'
+        except ObjectDoesNotExist:
+            return False
+    return False
 
-# Admin view
-@login_required
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-# Librarian view
-@login_required
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-# Member view
-@login_required
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
 # Authentication Views
 def login_view(request):
     if request.method == 'POST':
