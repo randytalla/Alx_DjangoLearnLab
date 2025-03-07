d8a2eaa9c19f6a33fbae677308b363558037b800
randytalla <randytala2@gmail.com>
2025-03-06 19:13:54 -0800

    created the book model

--- api_project/api_project/api/models.py
+++ api_project/api_project/api/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-# Create your models here.
-
 class Book(models.Model):
-    title = models.CharField(max_length=250)
-    author = models.CharField(max_length=100)
-    
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
     def __str__(self):
-        return f"{self.title}, {self.author}"
+        return f"{self.title} by {self.author}"
