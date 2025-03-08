b1e9dc70650d02f04a87e92a0b7edf8af1face2c
randytalla <randytala2@gmail.com>
2025-03-08 03:12:10 -0800

    Set up a view that uses the serializer to retrieve and return book data.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
