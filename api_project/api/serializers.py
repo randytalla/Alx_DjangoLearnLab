c1a38c0c833e30f90d7754d8b28084d1c307a026
randytalla <randytala2@gmail.com>
2025-03-08 03:08:20 -0800

    Set up a view that uses the serializer to retrieve and return book data.

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,7 +1,7 @@
 from .models import Book
 from rest_framework import serializers
 
-class BookSerializer(serializers.ModelSerializers):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
