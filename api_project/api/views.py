b84776c5a5beea488bd7c37cf097c8080ea086f1
randytalla <randytala2@gmail.com>
2025-03-08 03:18:15 -0800

    Set up a view that uses the serializer to retrieve and return book data.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,4 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView 
 from .models import Book
 from .serializers import BookSerializer
 
