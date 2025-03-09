d5b8857f576c6f5f4dda8831ad645754a9ddf180
randytalla <randytala2@gmail.com>
2025-03-09 11:38:02 -0700

    corrected the error concerning the listApiView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Book
-from .serializers import BookSerializer
+from .serializers import BookSerializers
 from .permissions import IsAdminOrReadOnly  # Import the custom permission
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -11,7 +11,7 @@ class BookViewSet(viewsets.ModelViewSet):
     - Write: Only Admin users can create, update, or delete
     """
     queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    serializer_class = BookSerializers
     permission_classes = [IsAdminOrReadOnly]  # Apply custom permission
 
 class BookList(generics.ListAPIView):
@@ -20,5 +20,5 @@ class BookList(generics.ListAPIView):
     Only authenticated users can access it.
     """
     queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    serializer_class = BookSerializers
     permission_classes = [IsAuthenticated]  # Only authenticated users can see the list
